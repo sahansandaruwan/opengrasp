a3ec867dcc74080a692b706e484ebc920849bf6e
Sahan Sandaruwan <84913793+sahansandaruwan@users.noreply.github.com.>
2026-08-11 19:34:20 +0530

    Update API and add gemini model

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello")
+from google import genai
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+geminiApiKey = os.getenv('GEMINI_API_KEY')
+
+client = genai.Client(api_key=geminiApiKey)
+
+interaction = client.interactions.create(model="gemini-3.6-flash", input="How are You")
+
+print(interaction.output_text)
