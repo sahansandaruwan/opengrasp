27c19f02813017a4bb41eac27b1d0de009dc3ad4
Sahan Sandaruwan <84913793+sahansandaruwan@users.noreply.github.com.>
2026-08-15 11:39:26 +0530

    update main.py

--- main.py
+++ main.py
@@ -2,18 +2,12 @@ from src import geminiTextResponse
 
 userInput = ""
 
-<<<<<<< HEAD
 while userInput == userInput:
     if userInput == "exit":
         break
     else:
         userInput = input("Type : ")
         print(geminiTextResponse(userInput))
-=======
-while userInput != "exit":
-    userInput = input("User : ")
-    print(geminiTextResponse(userInput))
->>>>>>> ff6728b42e75ec7c9de3b6d33df69ce3c62e0e7b
 
 
 
